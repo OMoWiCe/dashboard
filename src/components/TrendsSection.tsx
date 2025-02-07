f1ee647f68474fdcd80793826467dc68baf854ff
hashini-sugandika <130230222+hashini-sugandika@users.noreply.github.com>
2025-02-07 16:00:01 +0530

    fix: install mui package

--- src/components/TrendsSection.tsx
+++ src/components/TrendsSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent } from '@/components/ui/card';
+import { Card, CardContent } from '@mui/material';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 const dummyDailyData = [
