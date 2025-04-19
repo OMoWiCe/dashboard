cbdb9ed456232d1a773d3e3d77ac1ed9856f3b86
th3-s7r4ng3r <gvinura@gmail.com>
2025-04-19 19:26:24 +0530

    Ref: Removed analyzing packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import analyze from 'vite-bundle-analyzer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), analyze()],
+  plugins: [react()],
   build: {
     minify: true,
     sourcemap: false,
