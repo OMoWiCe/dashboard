e6b182813fb06a032f4e34fa9d510c4f98989a19
th3-s7r4ng3r <gvinura@gmail.com>
2025-05-14 12:44:00 +0530

    Del: Remove previous project and starting from scratch

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, Typography, Link } from '@mui/material';
-
-export default function Footer() {
-  return (
-    <Box sx={{ bgcolor: 'grey.800', color: 'white', p: 2, textAlign: 'center', mt: 4 }}>
-      <Typography>&copy; 2025 Your Company. <Link href="https://documentation.link" target="_blank" color="inherit">Documentation</Link></Typography>
-    </Box>
-  );
-}
